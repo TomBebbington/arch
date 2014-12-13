83e13f57d9cbaa707a75cb7bb77e90a6d77a2b34
Tom Bebbington <tophattedcoder@gmail.com>
2014-12-13 23:00:39 +0000

    Update reflector.sh

--- reflector.sh
+++ reflector.sh
@@ -5,7 +5,7 @@ set -e
 tmp=$(mktemp)
 
 echo -n 'Downloading mirror list... '
-reflector --country 'United States' --sort rate -n 5 --save "$tmp"
+reflector --country 'United Kingdom' --sort rate -n 5 --save "$tmp"
 echo done.
 
 cp --no-preserve=mode --backup=number "$tmp" /etc/pacman.d/mirrorlist
